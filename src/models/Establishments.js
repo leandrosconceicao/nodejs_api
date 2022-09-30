ab1285fa7a284724eaad29a2ad2b722ed481d975
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-09-29 23:46:27 -0300

    Criação do schema de dados dos estabelecimentos

---
+++ src/models/Establishments.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const establishmentsSchema = new mongoose.Schema({
+    _id: {type: Number},
+    storeName: {type: String},
+    stores: {
+        type: [{
+            id: String,
+            location: String,   
+        }],
+        default: undefined
+    },
+    ownerId: {type: String},
+}, {
+    versionKey: false,
+})
+
+const establishments = mongoose.model('estabelecimentos', establishmentsSchema);
+
+export default establishments;
