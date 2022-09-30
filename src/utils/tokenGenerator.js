478760de04fa585a5d8743286ab50b5040757571
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-09-29 23:48:01 -0300

    Inclusão de metodo para validação de token enviado nas requisições do client.

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -1,9 +1,12 @@
 import Jwt from 'jsonwebtoken';
-import users from '../models/Users.js';
 
 class TokenGenerator {
     static generate(users) {
         return Jwt.sign({id: users._id}, process.env.CHAVE_JWT);
+    }
+
+    static verify(token) {
+        return Jwt.verify(token, process.env.CHAVE_JWT);
     }
 }
 
