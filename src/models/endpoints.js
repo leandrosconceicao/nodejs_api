05250e30b9b6234ae526b6a2f13284b56f924016
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-09-29 23:45:53 -0300

    Adicionando link para o endpoint dos estabelecimentos

--- src/models/endpoints.js
+++ src/models/endpoints.js
@@ -3,6 +3,7 @@ class Endpoints {
     static home = '/';
     static products = '/products';
     static authentication = '/authentication';
+    static establishments = '/establishments';
     static users = '/users';
 }
 
