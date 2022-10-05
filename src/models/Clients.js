3c5fde54c827594b21a8ba561ad978f19c2b185e
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-10-05 15:33:18 -0300

    Refatorando model de cliente

--- src/models/Clients.js
+++ src/models/Clients.js
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
 
 const clientsSchema = new mongoose.Schema({
-    _id: {type: String},
-    name: {type: String, required: true},
-    cgc: {type: String, required: true},
-    email: {type: String, required: true},
+    _id: { type: String },
+    name: { type: String },
+    email: { type: String, required: true, unique: true},
+    password: { type: String},
+    isValid: { type: Boolean, default: undefined},
+    phoneNumber: { type: String },
     clientAddress: {
-        clientAddress: [{
-            id: String,
-            address: String,
-            complement: String,
-            number: String,
-            state: String,
-            zipCode: String,
-        }],
-    }
+        type: [{
+                id: String,
+                address: String,
+                complement: String,
+                number: String,
+                state: String,
+                zipCode: String,
+            }],
+        _id: false,
+        default: undefined,
+        version: false,
+    },
+    version: false,
 })
 
 const clients = mongoose.model('clients', clientsSchema)
