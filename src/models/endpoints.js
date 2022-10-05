72e892cf211fdbd497ccffa3d9d2dde9f868cb17
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-10-05 17:51:45 -0300

    Adicionado endpoint de busca de adicionais

--- src/models/endpoints.js
+++ src/models/endpoints.js
@@ -4,6 +4,7 @@ class Endpoints {
     static products = '/products';
     static product_images = `${this.products}/images`;
     static menu_items = `${this.products}/menu_items`;
+    static add_ones = `${this.products}/add_ones`;
     static categories = '/categories';
     static authentication = '/authentication';
     static establishments = '/establishments';
