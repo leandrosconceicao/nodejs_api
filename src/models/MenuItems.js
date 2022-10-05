46e91906bfb2e608e28595514c05f8e9c95b74be
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-10-05 17:06:45 -0300

    Schema de dados da agregação

---
+++ src/models/MenuItems.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const menuItemsSchema = new mongoose.Schema({
+    nome: {type: String},
+    ordenacao: {type: Number},
+    products: {
+        type: [{
+            descricao: String,
+            preco: Number,
+            produto: String
+        }],
+        default: undefined
+    }
+});
+
+const menuItems = mongoose.model('menuItems', menuItemsSchema, 'categorias')
+
+export default menuItems;
