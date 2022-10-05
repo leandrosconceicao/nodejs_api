eec86d1897497fc0f4b1adf44f094e567b5ac884
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-10-05 15:34:02 -0300

    Refatorando o controller de clientes.

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -1,52 +1,113 @@
 import Clients from "../models/Clients.js";
 import ApiResponse from "../models/ApiResponse.js";
+import TokenGenerator from "../utils/tokenGenerator.js";
+import PassGenerator from "../utils/passGenerator.js";
+import Jwt from "jsonwebtoken";
+import Validators from "../utils/utils.js";
 
 class ClientController {
-    static findAll = (req, res) => {
-        let client = new Clients(req.query);
-        Clients.find(client,(err, client) => {
-            if (err) {
-                res.status(500).json(ApiResponse.dbError(err));
+
+    static async findOne(req, res) {
+        try {
+            TokenGenerator.verify(req.headers.authorization);
+            res.status(200).json(ApiResponse.returnSucess(await Clients.findById(req.params.id)));
+        } catch (e) {
+            if (e.name === 'TokenExpiredError') {
+                res.status(401).json(ApiResponse.tokenExpired())
+            } else if (e instanceof Jwt.JsonWebTokenError) {
+                res.status(401).json(ApiResponse.unauthorized());
             } else {
-                console.log(client);
-                res.status(200).json(ApiResponse.returnSucess(client));
+                res.status(500).json(ApiResponse.dbError(e));
             }
-        })
+        }
     }
 
-    static add = (req, res) => {
-        let product = new Clients(req.body);
-        product.save((err) => {
-            if (err) {
-                res.status(500).send(ApiResponse.returnError())
-            } else {
-                res.status(201).json(ApiResponse.returnSucess())
+    static async findAll(req, res) {
+        try {
+            TokenGenerator.verify(req.headers.authorization);
+            let query = req.query;
+            if (!Validators.checkField(query.name)) {
+                delete query.name;
+            } else if (!Validators.checkField(query.cgc)) {
+                delete query.cgc;
+            } else if (!Validators.checkField(query.email)) {
+                delete query.email;
+            } else if (!Validators.checkField(query.isActive)) {
+                delete query.isActive;
             }
-        })
-        
+            let client = new Clients(query);
+            res.status(200).json(ApiResponse.returnSucess(await Clients.find(client)));
+        } catch (e) {
+            if (e.name === 'TokenExpiredError') {
+                res.status(401).json(ApiResponse.tokenExpired())
+            } else if (e instanceof Jwt.JsonWebTokenError) {
+                res.status(401).json(ApiResponse.unauthorized());
+            } else {
+                res.status(500).json(ApiResponse.dbError(e));
+            }
+        }
     }
 
-    static update = (req, res) => {
-        let id = req.body.id;
-        let data = req.body.data;
-        Clients.findByIdAndUpdate(id, {$set: data}, (err) => {
-            if (err) {
-                res.status(500).send(ApiResponse.dbError(err));
-            } else {
-                res.status(200).json(ApiResponse.returnSucess())
+    static async add(req, res) {
+        try {
+            TokenGenerator.verify(req.headers.authorization);
+            let client = new Clients(req.body);
+            client.password = new PassGenerator(client.password).build();
+            res.status(200).json(ApiResponse.returnSucess(await client.save()))
+        } catch (e) {
+            if (e.code === 11000) {
+                res.status(400).json(ApiResponse.returnError(`Atenção Usuário já cadastrado.`))
             }
-        })
+            else if (e.name === 'TokenExpiredError') {
+                res.status(401).json(ApiResponse.tokenExpired())
+            } else if (e instanceof Jwt.JsonWebTokenError) {
+                res.status(401).json(ApiResponse.unauthorized());
+            } else {
+                res.status(500).json(ApiResponse.dbError(e));
+            }
+        }
+
     }
 
-    static delete = (req, res) => {
-        let id = req.body.id;
-        Clients.findByIdAndDelete(id, (err) => {
-            if (err) {
-                res.status(500).json(ApiResponse.dbError(err))
+    static async update(req, res) {
+        try {
+            let id = req.body.id;
+            let data = req.body.data;
+            const db = await Clients.findByIdAndUpdate(id, { $set: data })
+            if (db) {
+                res.status(200).json(ApiResponse.returnSucess(db));
             } else {
-                res.status(200).json(ApiResponse.returnSucess())
+                res.status(400).json(ApiResponse.returnError('Nenhum dado atualizado, verifique os filtros e tente novamente.'))
             }
-        })
+        } catch (e) {
+            if (e.name === 'TokenExpiredError') {
+                res.status(401).json(ApiResponse.tokenExpired())
+            } else if (e instanceof Jwt.JsonWebTokenError) {
+                res.status(401).json(ApiResponse.unauthorized());
+            } else {
+                res.status(500).json(ApiResponse.dbError(e));
+            }
+        }
+    }
+
+    static async delete(req, res) {
+        try {
+            let id = req.body.id;
+            const db = await Clients.findByIdAndDelete(id);
+            if (db) {
+                res.status(200).json(ApiResponse.returnSucess(db))
+            } else {
+                res.status(400).json(ApiResponse.returnError('Nenhum dado excluido, verifique os filtros e tente novamente'))
+            }
+        } catch (e) {
+            if (e.name === 'TokenExpiredError') {
+                res.status(401).json(ApiResponse.tokenExpired())
+            } else if (e instanceof Jwt.JsonWebTokenError) {
+                res.status(401).json(ApiResponse.unauthorized());
+            } else {
+                res.status(500).json(ApiResponse.dbError(e));
+            }
+        }
     }
 }
 
