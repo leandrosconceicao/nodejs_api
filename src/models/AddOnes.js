c7e6b2da7f47dff757c909b0819b4511003b5f63
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-11-13 00:31:26 -0300

    Ajustes na estrutura das informações de adicionais

--- src/models/AddOnes.js
+++ src/models/AddOnes.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 
 const addOneSchema = new mongoose.Schema({
-    _id: {type: String},
-    storeCode: {type: String},
-    maxQuantityAllowed: {type: Number},
-    name: {type: String},
-    price: {type: Number},
-    isRequired: {type: Boolean},
+  _id: { type: String },
+  storeCode: { type: String },
+  name: { type: String },
+  price: { type: Number },
+  maxQtdAllowed: { type: Number },
+  items: {
+    type: [
+      {
+        name: String,
+        price: Number,
+      },
+    ],
+    default: undefined,
+  },
 });
 
-const addOnes = mongoose.model('addOnes', addOneSchema)
+const addOnes = mongoose.model("addOnes", addOneSchema);
 
 export default addOnes;
