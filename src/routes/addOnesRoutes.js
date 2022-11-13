9c27126bd5251eb68f27e517e8f2869c6c40a1b2
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-11-13 00:32:29 -0300

    Adicionando metodo para inclusão e exclusão de item adicionado aos adicionais

--- src/routes/addOnesRoutes.js
+++ src/routes/addOnesRoutes.js
@@ -8,6 +8,7 @@ router
     .get(endpoints.add_ones, AddOneController.findAll)
     .post(endpoints.add_ones, AddOneController.add)
     .put(endpoints.add_ones, AddOneController.update)
+    .patch(endpoints.add_ones, AddOneController.patch)
     .delete(endpoints.add_ones, AddOneController.delete)
 
 export default router;
