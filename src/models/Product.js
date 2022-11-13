f5fbacc38b4e9d656c6682c30539c2ebcd3e6197
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-11-13 00:32:50 -0300

    Regra de negocio modificada para acomodar dados dos adicionais

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,34 +1,45 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    _id: {type: Number},
-    isActive: {type: Boolean, required: true},
-    categoria: {type: String},
-    categoryId: {type: Number},
-    preco: {type: Number},
-    produto: {type: String, required: true},
-    descricao: {type: String},
-    preparacao: Boolean,
-    storeCode: {type: String, required: true},
-    addONes: {
-        type: [{
-            _id: String,
-            isRequired: Boolean,
-            maxQuantityAllowed: Number,
-            name: String,
-            price: Number,
-            selectedQuantity: Number,
-            storeCode: String,
-            totalValue: Number
-        }],
-        default: undefined
-    },
-    image: {
-        name: {type: String, default: undefined},
-        link: {type: String, default: undefined},
-    }
+  _id: { type: Number },
+  isActive: { type: Boolean, required: true },
+  categoria: { type: String },
+  categoryId: { type: Number },
+  preco: { type: Number },
+  produto: { type: String, required: true },
+  descricao: { type: String },
+  preparacao: Boolean,
+  storeCode: { type: String, required: true },
+  addOnes: {
+    type: [
+      {
+        isRequired: Boolean,
+        productsAddOnes: {
+          _id: String,
+          storeCode: String,
+          name: String,
+          price: Number,
+          maxQtdAllowed: Number,
+          items: {
+            type: [
+              {
+                name: String,
+                price: Number,
+              },
+            ],
+            default: undefined,
+          },
+        },
+      },
+    ],
+    default: undefined,
+  },
+  image: {
+    name: { type: String, default: undefined },
+    link: { type: String, default: undefined },
+  },
 });
 
-const products = mongoose.model('produtos', productSchema)
+const products = mongoose.model("produtos", productSchema);
 
 export default products;
