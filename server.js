62c6a99d90100b27cba98d9164acd8a4f188c43f
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-11-23 10:16:16 -0300

    Inicio da implementação do uso do socket na api

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import app from './src/app.js';
+import {createServer} from 'http';
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {})
+
+io.on('connection', socket => {
+    console.log('Socket conectado', socket.id);
+    socket.on("sendMessage", data => {
+        console.log(data);
+    });
+    socket.on("disconnect", (reason) => {
+        console.log(reason);
+    })
+})
+
 
 const port = process.env.PORT || 8081
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
+
+httpServer.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
