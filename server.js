28e7cc72095f921068e663e9c0314aef289702a0
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-12-13 23:23:18 -0300

    Ajuste no server

--- server.js
+++ server.js
@@ -7,18 +7,9 @@ import { Server } from "socket.io";
 const httpServer = createServer(app);
 const io = new Server(httpServer, {})
 
-io.on('connection', socket => {
-    console.log('Socket conectado', socket.id);
-    socket.on("sendMessage", data => {
-        console.log(data);
-    });
-    socket.on("disconnect", (reason) => {
-        console.log(reason);
-    })
-})
-
-
 const port = process.env.PORT || 80
 
 
-httpServer.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
+httpServer.listen(port, "0.0.0.0")
+
+export default io;
