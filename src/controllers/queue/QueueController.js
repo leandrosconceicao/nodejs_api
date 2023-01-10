344ffb8bf126e942cfd022955464c8eab42bc03d
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-01-10 20:53:07 -0300

    Novos metodos

--- src/controllers/queue/QueueController.js
+++ src/controllers/queue/QueueController.js
@@ -1,44 +1,77 @@
-import ApiResponse from '../../models/ApiResponse.js';
-import Queue from '../../models/Queue.js';
-import Validators from '../../utils/utils.js';
+import Queue from "../../models/Queue.js";
+import ApiResponse from "../../models/ApiResponse.js";
+import Validators from "../../utils/utils.js";
+
 class QueueService {
 
     static findAll = (req, res) => {
         let storeCode = req.query.storeCode;
         if (!Validators.checkField(storeCode)) {
-            res.status(400).json(ApiResponse.parameterNotFound('(branch)'));
+            res.status(406).json(ApiResponse.parameterNotFound('(storeCode)'))
         } else {
-            Queue.find({storeCode: storeCode}, (err, queue) => {
+            Queue.find({storeCode: storeCode}, (err, data) => {
                 if (err) {
                     res.status(500).json(ApiResponse.dbError(err));
                 } else {
-                    res.status(200).json(ApiResponse.returnSucess(queue));
+                    res.status(200).json(ApiResponse.returnSucess(data))
                 }
             })
         }
     }
 
-    static add = (req, res) => {
-        let storeCode = req.body.storeCode;
+    static generate = async (req, res) => {
+        const storeCode = req.params.storeCode;
         if (!Validators.checkField(storeCode)) {
-            res.status(400).json(ApiResponse.parameterNotFound('(storeCode)'));
+            res.status(406).json(ApiResponse.parameterNotFound('(storeCode)'));
         } else {
-            Queue.find({storeCode: storeCode}, (err, queue) => {
+            const queue = await Queue.find({storeCode: storeCode});
+            if (!queue.length) {
+                let newQueue = new Queue({
+                    date: new Date().toISOString(),
+                    position: 1,
+                    storeCode: storeCode,
+                });
+                newQueue.save((err, q) => {
+                    if (err) {
+                        res.status(500).json(ApiResponse.dbError(err));
+                    } else {
+                        res.status(201).json(ApiResponse.returnSucess([q]));
+                    }
+                })
+            } else {
+                let lastOnLine = queue.slice(-1)[0];
+                let newQueue = new Queue({
+                    date: new Date().toISOString(),
+                    position: lastOnLine.position + 1,
+                    storeCode: storeCode,
+                })
+                newQueue.save((err) => {
+                    if (err) {
+                        res.status(500).json(ApiResponse.dbError(err));
+                    } else {
+                        Queue.find({storeCode: storeCode}, (err, data) => {
+                            if (err) {
+                                res.status(500).json(ApiResponse.dbError(err));
+                            } else {
+                                res.status(200).json(ApiResponse.returnSucess(data));
+                            }
+                        }).sort({ position: -1 }).limit(5)
+                    }
+                })
+            }
+        }
+    }
+
+    static delete = (req, res) => {
+        let storeCode = req.params.storeCode;
+        if (!Validators.checkField(storeCode)) {
+            res.status(406).json(ApiResponse.parameterNotFound('(storeCode)'));
+        } else {
+            Queue.deleteMany({storeCode: storeCode}, (err) => {
                 if (err) {
                     res.status(500).json(ApiResponse.dbError(err));
                 } else {
-                    const newQueue = new Queue({
-                        position: queue.position++,
-                        date: new Date().toLocaleDateString(),
-                        storeCode: storeCode,
-                    });
-                    newQueue.save((err, q) => {
-                        if (err) {
-                            res.status(500).json(ApiResponse.dbError(err));
-                        } else {
-                            res.status(201).json(ApiResponse.returnSucess(q));
-                        }
-                    })
+                    res.status(200).json(ApiResponse.returnSucess());
                 }
             })
         }
