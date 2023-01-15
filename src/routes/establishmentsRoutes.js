756ec2af89a5b03a28516b81a1f8d5b96904ea8d
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-01-15 16:46:52 -0300

    Retirando validação de token

--- src/routes/establishmentsRoutes.js
+++ src/routes/establishmentsRoutes.js
@@ -5,7 +5,7 @@ import validateToken from '../middlewares/tokenController.js';
 const router = express.Router();
 
 router
-    .get(endpoints.establishments, validateToken,  EstablishmentsController.findAll)
+    .get(endpoints.establishments,  EstablishmentsController.findAll)
     .get(`${endpoints.establishments}/:id`, EstablishmentsController.findOne)
     .post(endpoints.establishments, validateToken, EstablishmentsController.add)
     .delete(endpoints.establishments, validateToken, EstablishmentsController.del)
