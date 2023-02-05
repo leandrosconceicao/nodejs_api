ac17ad79b8b9662fc8a4b438fe5848a029a45e22
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-02-05 07:42:40 -0300

    Metodo para geração de id

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -1,5 +1,6 @@
 import Jwt from 'jsonwebtoken';
 import crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 class TokenGenerator {
     static generate(users) {
         return Jwt.sign({id: users._id}, process.env.CHAVE_JWT, {expiresIn: 86400});
@@ -11,6 +12,10 @@ class TokenGenerator {
 
     static getToken() {
         return crypto.randomBytes(20).toString('hex');
+    }
+
+    static generateId() {
+        return uuidv4();
     }
 
 
