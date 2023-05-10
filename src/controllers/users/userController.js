e9ce96446999393ac5f594cf0b5ce34e970d993e
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-05-10 14:53:46 -0300

    Refatorando controlador para utilizar o middware de exceptions

--- src/controllers/users/userController.js
+++ src/controllers/users/userController.js
@@ -4,60 +4,63 @@ import ApiResponse from "../../models/ApiResponse.js";
 import TokenGenerator from "../../utils/tokenGenerator.js";
 import PassGenerator from "../../utils/passGenerator.js";
 import Validators from "../../utils/utils.js";
+import NotFoundError from "../errors/NotFoundError.js";
 
 class UserController {
-  static add = (req, res) => {
+  static add = (req, res, next) => {
     let user = new Users(req.body);
     user.pass = new PassGenerator(user.pass).build();
     user.save((err, users) => {
       if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
+        next(err);
       } else {
-        res.status(201).json(ApiResponse.returnSucess(users));
+        ApiResponse.returnSucess(users).sendResponse(res);
       }
     });
   };
 
-  static delete = (req, res) => {
+  static delete = (req, res, next) => {
     let id = req.body.id;
     Users.findByIdAndDelete(id, (err) => {
       if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
+        next(err);
       } else {
-        res.status(200).json(ApiResponse.returnSucess());
+        ApiResponse.returnSucess().sendResponse(res);
       }
     });
   };
 
-  static update = (req, res) => {
+  static update = (req, res, next) => {
     let body = req.body;
     Users.findByIdAndUpdate(body.id, { $set: body.data }, (err) => {
       if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
+        next(err);
       } else {
-        res.status(200).json(ApiResponse.returnSucess());
+        ApiResponse.returnSucess().sendResponse(res);
       }
     });
   };
 
-  static findAll = (req, res) => {
-    Users.find(req.query, (err, users) => {
-      if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
-      } else {
-        res.status(200).json(ApiResponse.returnSucess(users));
+  static findAll = async (req, res, next) => {
+    try {
+      const users = await Users.find(req.query);
+      if (!users) {
+        throw new NotFoundError("Busca não localizou dados");
       }
-    });
+      return ApiResponse.returnSucess(users).sendResponse(res);
+    } catch (e) {
+      next(e);
+    }
   };
 
-  static async authenticate(req, res) {
+  static async authenticate(req, res, next) {
     try {
       let body = req.body;
       if (!Validators.checkField(body.email)) {
-        return res.status(406).json(ApiResponse.parameterNotFound('(email)'));
+        return ApiResponse.parameterNotFound('(email)').sendResponse(res);
       }
       if (!Validators.checkField(body.password)) {
-        return res.status(406).json(ApiResponse.parameterNotFound('(password)'));
+        return ApiResponse.parameterNotFound('(password)').sendResponse(res);
       }
       const hashPass = new PassGenerator(body.password).build();
       let users = await Users.findOne({
@@ -65,17 +68,17 @@ class UserController {
         pass: hashPass,
       });
       if (!users) {
-        return res.status(400).json(ApiResponse.badRequest("Dados incorretos ou inválidos."));
+        throw new NotFoundError("Dados incorretos ou inválidos.")
       } else {
         let ests = await Establishments.find({_id: {$in: users.establishments}})
         users.ests = ests;
         const token = TokenGenerator.generate(body.email);
         res.set("Authorization", token);
         res.set("Access-Control-Expose-Headers", "*");
-        return res.status(200).json(ApiResponse.returnSucess(users));
+        return ApiResponse.returnSucess(users).sendResponse(res);
       }
     } catch (e) {
-      return res.status(500).json(ApiResponse.badRequest(e));
+      next(e);
     }
   }
 }
