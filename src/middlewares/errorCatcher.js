e09bfcc8101d8279d64a860334854efe4b3615f1
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-05-10 13:54:31 -0300

    Incluindo validações no error catcher

--- src/middlewares/errorCatcher.js
+++ src/middlewares/errorCatcher.js
@@ -1,11 +1,17 @@
+import mongoose from 'mongoose';
 import NotFoundError from '../controllers/errors/NotFoundError.js';
 import ApiResponse from '../models/ApiResponse.js';
 import Jwt from "jsonwebtoken";
+import ValidationError from '../controllers/errors/ValidationError.js';
 
 // eslint-disable-next-line no-unused-vars
 function errorCatcher(err, req, res, next) {
     console.error(err);
-    if (err instanceof Jwt.JsonWebTokenError) {
+    if (err instanceof mongoose.Error.CastError) {
+        ApiResponse.badRequest().sendResponse(res)
+    } else if (err instanceof mongoose.Error.ValidationError) {
+        new ValidationError(err).sendResponse(res);
+    } else if (err instanceof Jwt.JsonWebTokenError) {
         if (err.name === 'TokenExpiredError') {
             ApiResponse.tokenExpired().sendResponse(res);
         }
