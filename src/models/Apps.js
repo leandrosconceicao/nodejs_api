21399a7ced1fe651f99d0f726ee1306a0790089b
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-05-10 21:30:15 -0300

    Adicionando mensagem padrão para campos requeridos

--- src/models/Apps.js
+++ src/models/Apps.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
 const appsSchema = new mongoose.Schema({
-    appsName: {type: String},
-    version: {type: String},
-    releaseDate: {type: Date}
+    appsName: {type: String,
+        required: [true, "Parametro (appsName) é obrigatório"]
+    },
+    version: {type: String,
+        required: [true, "Parametro (version) é obrigatório"]
+    },
+    releaseDate: {type: Date,
+        required: [true, "Parametro (releaseDate) é obrigatório"]
+    }
 })
 
 const apps = mongoose.model('apps', appsSchema)
