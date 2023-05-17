d92385ee207be770926c8a6e400084e0e97e604e
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-05-17 15:11:52 -0300

    Definindo middleware pare paginação e filtros

---
+++ src/middlewares/paginationAndFilters.js
@@ -0,0 +1,20 @@
+import ApiResponse from "../models/ApiResponse.js";
+import Headers from "../controllers/base/Headers.js";
+
+async function paginationAndFilters(req, res, next) {
+    try {
+        const headers = new Headers(req.headers);
+        const paginationConfig = headers.getPagination();
+        const ordenationConfig = headers.getOrderBy();
+        const request = req.query;
+        const parsedData = await request.find()
+            .sort({[ordenationConfig.orderBy]: headers.ordenation})
+            .skip(paginationConfig.pagination)
+            .limit(paginationConfig.limit);
+        ApiResponse.returnSucess(parsedData).sendResponse(res);
+    } catch (e) {
+        next(e);
+    }
+}
+
+export default paginationAndFilters;
