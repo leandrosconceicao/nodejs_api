6b80e210263d838508fab921efcf5aa0db7f02c7
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-05-17 21:51:55 -0300

    Adicionando mensagem padrão nos parametros obrigatórios

--- src/models/Categories.js
+++ src/models/Categories.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const categorieSchema = new mongoose.Schema({
     _id: {type: Number},
-    nome: {type: String},
-    storeCode: {type: String, required: true},
-    ordenacao: {type: Number, required: true},
+    nome: {type: String, required: [true, "Parametro (nome) é obrigatório"]},
+    storeCode: {type: String, required: [true, "Parametro (storeCode) é obrigatório"]},
+    ordenacao: {type: Number, required: [true, "Parametro (ordenacao) é obrigatório"]},
     image: {type: String}
 });
 
