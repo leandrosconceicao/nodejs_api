b305853efc67e00c1482def7faba40a449c0dc5b
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-05-18 21:39:58 -0300

    Inclusão de middware na rota de consulta de pedidos

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 import OrdersController from "../controllers/orders/ordersController.js";
 import Endpoints from "../models/Endpoints.js";
 import validateToken from "../middlewares/tokenController.js";
+import paginationAndFilters from "../middlewares/paginationAndFilters.js";
 
 const router = express.Router();
 
 router
-    .get(Endpoints.orders, OrdersController.findAll)
+    .get(Endpoints.orders, OrdersController.findAll, paginationAndFilters)
     .get(`${Endpoints.orders}/:pedidosId`, OrdersController.findOne)
     .post(Endpoints.orders, OrdersController.post)
     .put(Endpoints.orders, validateToken, OrdersController.update)
