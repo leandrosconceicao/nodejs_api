9d7ff2a2ab56c5123d20d18593ac284327a518e1
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-08 22:29:12 -0300

    inclusão do middleware de filtros e paginação

--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 import ClientController from '../controllers/clients/clientsController.js';
 import endpoints from "../models/Endpoints.js"
 import validateToken from '../middlewares/tokenController.js';
+import paginationAndFilters from '../middlewares/paginationAndFilters.js';
 const router = express.Router();
 
 router
-    .get(endpoints.clients, validateToken, ClientController.findAll)
+    .get(endpoints.clients, validateToken, ClientController.findAll, paginationAndFilters)
     .get(`${endpoints.clients}/:id`, ClientController.findOne)
     .post(endpoints.clients, ClientController.add)
     .post(`${endpoints.clients}/authentication`, ClientController.authentication)
