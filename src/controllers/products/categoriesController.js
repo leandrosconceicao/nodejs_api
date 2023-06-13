f130aa3f5a94f12dd6f4c963e54e12e578d5d936
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-13 11:35:49 -0300

    Refatoração dos metodos

--- src/controllers/products/categoriesController.js
+++ src/controllers/products/categoriesController.js
@@ -1,62 +1,104 @@
 import Category from "../../models/Categories.js";
 import ApiResponse from "../../models/ApiResponse.js";
 import Validators from "../../utils/utils.js";
+import RegexBuilder from "../../utils/regexBuilder.js";
+import ProductController from "./productController.js";
 
 class CategoryController {
-  static findAll = (req, res) => {
-    let search = req.query;
-    let cat = {};
-    if (Validators.checkField(search.id)) {
-      cat._id = search.id;
-    } else if (Validators.checkField(search.storeCode)) {
-      cat.storeCode = search.storeCode;
-    } else if (Validators.checkField(search.nome)) {
-      cat.nome = search.nome;
+  static async findAll(req, res, next) {
+    try {
+      let {id, storeCode, nome} = req.query;
+      let cat = {};
+      if (Validators.checkField(id)) {
+        cat._id = id;
+      }
+      if (Validators.checkField(storeCode)) {
+        cat.storeCode = storeCode;
+      }
+      if (Validators.checkField(nome)) {
+        cat.nome = RegexBuilder.searchByName(nome);
+      }
+      req.query = Category.find(cat);
+      next();
+    } catch (e) {
+      next(e);
     }
-    Category.find(cat, (err, result) => {
-      if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
-      } else {
-        res.status(200).json(ApiResponse.returnSucess(result));
-      }
-    });
-  };
+  }
 
-  static add = (req, res) => {
-    let category = new Category(req.body);
-    category.save((err) => {
-      if (err) {
-        console.log(1);
-        res.status(500).json(ApiResponse.dbError(err));
+  static async add(req, res, next) {
+    try {
+      let category = new Category(req.body);
+      let lastCategory = await CategoryController.getLastCategory(category.storeCode);
+      if (lastCategory) {
+        category.ordenacao = lastCategory.ordenacao + 1;
       } else {
-        console.log(2);
-        res.status(201).json(ApiResponse.returnSucess());
+        category.ordenacao = 1;
       }
-    });
-  };
+      await category.save();
+      return ApiResponse.returnSucess().sendResponse(res);
+    } catch (e) {
+      next(e);
+    }
+  }
 
-  static update = (req, res) => {
-    let id = req.body.id;
-    let data = req.body.data;
-    Category.findByIdAndUpdate(id, { $set: data }, (err) => {
-      if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
-      } else {
-        res.status(200).json(ApiResponse.returnSucess());
-      }
-    });
-  };
+  static async getLastCategory(storeCode) {
+    try {
+      const categories = await Category.find({storeCode: storeCode});
+      return categories.splice(-1)[0];
+    } catch (e) {
+      return null;
+    }
+  }
 
-  static delete = (req, res) => {
-    let id = req.query.id;
-    Category.findByIdAndDelete(id, (err) => {
-      if (err) {
-        res.status(500).json(ApiResponse.dbError(err));
-      } else {
-        res.status(200).json(ApiResponse.returnSucess());
+  static async updateName(req, res, next) {
+    try {
+      let {id, name} = req.body;
+      if (!Validators.checkField(id)) {
+        return ApiResponse.parameterNotFound("id").sendResponse(res);
       }
-    });
-  };
+      if (!Validators.checkField(name)) {
+        return ApiResponse.parameterNotFound("name").sendResponse(res);
+      }
+      await Category.findByIdAndUpdate(id, {"nome": name})
+      return ApiResponse.returnSucess().sendResponse(res);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async changeOrder(req, res, next) {
+    try {
+      const from = req.body.from;
+      const to = req.body.to;
+      if (!Validators.checkField(from._id) && !Validators.checkField(from.ordenacao)) {
+        return ApiResponse.parameterNotFound('from').sendResponse(res);
+      }
+      if (!Validators.checkField(to._id) && !Validators.checkField(to.ordenacao)) {
+        return ApiResponse.parameterNotFound('to').sendResponse(res);
+      }
+      await Category.findByIdAndUpdate(from._id, {"ordenacao": to.ordenacao});
+      await Category.findByIdAndUpdate(to._id, {"ordenacao": from.ordenacao});
+      return ApiResponse.returnSucess().sendResponse(res);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async delete(req, res, next) {
+    try {
+      let id = req.body.id;
+      if (!Validators.checkField(id)) {
+        return ApiResponse.parameterNotFound('id').sendResponse(res);
+      }
+      if (await ProductController.productsHasCategory(id)) {
+        return ApiResponse.badRequest('Categoria não pode ser excluida se possuir produtos cadastrados').sendResponse(res);
+      }
+      await Category.findByIdAndDelete(id);
+      return ApiResponse.returnSucess().sendResponse(res);
+    } catch (e) {
+      next(e);
+    }
+  }
 }
 
 export default CategoryController;
