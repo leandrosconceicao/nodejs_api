35b4dac43c9501bde3efa3ccc2842f6a946b98ea
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-15 09:22:02 -0300

    Ajustes na função de tratamento de exceções

--- src/middlewares/errorCatcher.js
+++ src/middlewares/errorCatcher.js
@@ -3,23 +3,30 @@ import NotFoundError from '../controllers/errors/NotFoundError.js';
 import ApiResponse from '../models/ApiResponse.js';
 import Jwt from "jsonwebtoken";
 import ValidationError from '../controllers/errors/ValidationError.js';
+import DuplicateError from '../controllers/errors/DuplicateError.js';
 
 // eslint-disable-next-line no-unused-vars
 function errorCatcher(err, req, res, next) {
     console.error(err);
     if (err instanceof mongoose.Error.CastError) {
         return ApiResponse.badRequest().sendResponse(res)
-    } else if (err instanceof mongoose.Error.ValidationError) {
+    } 
+    if (err instanceof mongoose.Error.ValidationError) {
         return new ValidationError(err).sendResponse(res);
-    } else if (err instanceof Jwt.JsonWebTokenError) {
+    }
+    if (err instanceof Jwt.JsonWebTokenError) {
         if (err.name === 'TokenExpiredError') {
             return ApiResponse.tokenExpired().sendResponse(res);
         }
         if (err instanceof Jwt.JsonWebTokenError) {
             return ApiResponse.unauthorized().sendResponse(res);
         }
-    } else if (err instanceof NotFoundError) {
+    } 
+    if (err instanceof NotFoundError) {
         return err.sendResponse(res);
+    }
+    if (err.code == 11000) {
+        return new DuplicateError(err).sendResponse(res);
     } else {    
         return ApiResponse.dbError(err).sendResponse(res);
     }
