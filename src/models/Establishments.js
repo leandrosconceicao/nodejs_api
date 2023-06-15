64237f8947a6edaa2fb682630d461a6a06ded731
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-15 09:22:25 -0300

    Ajustes nas propriedades do model de establishments

--- src/models/Establishments.js
+++ src/models/Establishments.js
@@ -1,28 +1,27 @@
 import mongoose from "mongoose";
+import TokenGenerator from "../utils/tokenGenerator.js";
 
 const establishmentsSchema = new mongoose.Schema({
-    _id: {type: Number},
-    storeName: {type: String},
+    name: {type: String, required: [true, "Parametro (name) é obrigatório"] },
+    createDate: {type: Date, default: new Date()},
     stores: {
-        type: [{
-            id: String,
-            location: String,   
-            url: String,
-            isOpen: Boolean,
-        }],
-        default: undefined
+        type: [
+            new mongoose.Schema({
+            _id: {type: String, default: TokenGenerator.generateId()},
+            createDate: {type: Date, default: new Date()},
+            location: {type: String, default: ""},   
+            url: {type: String, default: ""},
+            isOpen: {type: Boolean, default: false},
+        })
+    ],
+        default: []
     },
-    ownerId: {type: String},
+    ownerId: {type: String, unique: true, required: [true, "Parametro (ownerId) é obrigatório"]},
     logo: {type: String},
-    visibles: {
-        showCardInfo: {type: Boolean},
-        showSocialNetWork: {type: Boolean},
-        showTipInfo: {type: Boolean},
-    }
 }, {
     versionKey: false,
 })
 
-const establishments = mongoose.model('estabelecimentos', establishmentsSchema);
+const establishments = mongoose.model('establishments', establishmentsSchema);
 
 export default establishments;
