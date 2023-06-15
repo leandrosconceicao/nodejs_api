df4393263c94470e5529480839a03bab9a230afa
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-15 09:23:29 -0300

    Ajustando rota

--- src/routes/establishmentsRoutes.js
+++ src/routes/establishmentsRoutes.js
@@ -2,13 +2,14 @@ import express from 'express';
 import EstablishmentsController from '../controllers/establishments/establishmentsController.js';
 import endpoints from "../models/Endpoints.js"
 import validateToken from '../middlewares/tokenController.js';
+import paginationAndFilters from "../middlewares/paginationAndFilters.js";
 const router = express.Router();
 
 router
-    .get(endpoints.establishments,  EstablishmentsController.findAll)
+    .get(endpoints.establishments,  EstablishmentsController.findAll, paginationAndFilters)
     .get(`${endpoints.establishments}/:id`, EstablishmentsController.findOne)
     .post(endpoints.establishments, validateToken, EstablishmentsController.add)
     .delete(endpoints.establishments, validateToken, EstablishmentsController.del)
-    .patch(endpoints.establishments, validateToken, EstablishmentsController.patch)
+    .patch(endpoints.establishments, validateToken, EstablishmentsController.manageStore)
 
 export default router;
