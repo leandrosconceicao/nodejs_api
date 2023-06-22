a0bda7eddce339650be0f574e939b61c0b232042
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-22 19:05:53 -0300

    Router para as rotas relacionadas as contas

---
+++ src/routes/accountRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import AccountsController from "../controllers/accounts/accountsController.js";
+import Endpoints from "../models/Endpoints.js";
+import validateToken from "../middlewares/tokenController.js";
+import paginationAndFilters from "../middlewares/paginationAndFilters.js";
+
+const router = express.Router();
+
+router
+    .get(Endpoints.accounts, AccountsController.findAll, paginationAndFilters)
+    .get(`${Endpoints.accounts}/:id`, AccountsController.findOne)
+    .post(Endpoints.accounts, validateToken, AccountsController.post)
+
+export default router;
