1f51ee7b6d99c0d5c1799a6b09fa0e4c2bf1770b
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-06-22 19:06:47 -0300

    Rota para o novo endpoint de relatório

--- src/routes/reportsRoutes.js
+++ src/routes/reportsRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router
     .get(Endpoints.quantSales, validateToken, ReportsControllers.quantifySales)
+    .get(Endpoints.quantSalesByProduct, validateToken, ReportsControllers.quantifySalesByProduct)
 
 export default router;
