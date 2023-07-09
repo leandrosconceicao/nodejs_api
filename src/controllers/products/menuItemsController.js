3760f8142c936f1847c9e046d6ff0905ddc97b26
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-07-09 01:04:09 -0300

    Ajuste na aggregation

--- src/controllers/products/menuItemsController.js
+++ src/controllers/products/menuItemsController.js
@@ -1,59 +1,58 @@
+import mongoose from "mongoose";
 import ApiResponse from "../../models/ApiResponse.js";
 import MenuItems from "../../models/MenuItems.js";
 import Validators from "../../utils/utils.js";
+import InvalidParameter from "../errors/InvalidParameter.js";
+var ObjectId = mongoose.Types.ObjectId;
 
 class menuItemsController {
-    static async get(req, res) {
-        try {
-            let storeCode = req.query.storeCode;
-            if (Validators.checkField(storeCode)) {
-                const data = await MenuItems.aggregate([
-                    {
-                        $match: {
-                            storeCode: storeCode
-                        }
-                    }, {
-                        $lookup: {
-                            from: "produtos",
-                            localField: "_id",
-                            foreignField: "categoryId",
-                            as: "products"
-                        }
-                    }, {
-                        $project: {
-                            _id: 0,
-                            image: 0,
-                            storeCode: 0,
-                            // "products._id": 0,
-                            "products.preparacao": 0,
-                            "products.categoria": 0,
-                            "products.storeCode": 0,
-                            "products.status": 0,
-                            "products.dataInsercao": 0,
-                            "products.categoryId": 0
-                        }
-                    }, {
-                        $match: {
-                            "products.isActive": true
-                        }
-                    }, {
-                        $sort: {
-                            ordenacao: 1
-                        }
-                    }, {
-                        $project: {
-                            "products.isActive": 0
-                        }
-                    }
-                ]);
-                res.status(200).json(ApiResponse.returnSucess(data));
-            } else {
-                res.status(400).json(ApiResponse.parameterNotFound('StoreCode'));
-            }
-        } catch (e) {
-            res.status(500).json(ApiResponse.dbError(e));
-        }
+  static async get(req, res, next) {
+    try {
+      let storeCode = req.query.storeCode;
+      if (!Validators.checkField(storeCode)) {
+        throw new InvalidParameter('storeCode');
+      } else {
+        const data = await MenuItems.aggregate([
+            {
+              $match: {
+                  storeCode: new ObjectId(storeCode)
+              }
+            },
+            {
+              $lookup: {
+                from: "products",
+                localField: "_id",
+                foreignField: "category",
+                as: "products",
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                storeCode: 0,
+                "products.preparacao": 0,
+                "products.categoria": 0,
+                "products.storeCode": 0,
+                "products.status": 0,
+                "products.dataInsercao": 0,
+                "products.categoryId": 0,
+                "products.category": 0,
+                "products.createDate": 0,
+                "products.isActive": 0,
+              },
+            },
+            {
+              $sort: {
+                ordenacao: 1,
+              },
+            },
+          ]);
+          res.status(200).json(ApiResponse.returnSucess(data));
+      }
+    } catch (e) {
+      next(e);
     }
+  }
 }
 
 export default menuItemsController;
