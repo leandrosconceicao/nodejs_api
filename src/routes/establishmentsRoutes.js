57abd468dcd63af02e067d0f9be55b047dc539d0
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-07-17 22:59:17 -0300

    Alteração de verbo

--- src/routes/establishmentsRoutes.js
+++ src/routes/establishmentsRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import EstablishmentsController from '../controllers/establishments/establishmentsController.js';
-import endpoints from "../models/Endpoints.js"
+import Endpoints from "../models/Endpoints.js"
 import validateToken from '../middlewares/tokenController.js';
 import paginationAndFilters from "../middlewares/paginationAndFilters.js";
 const router = express.Router();
 
 router
-    .get(endpoints.establishments,  EstablishmentsController.findAll, paginationAndFilters)
-    .get(`${endpoints.establishments}/:id`, EstablishmentsController.findOne)
-    .post(endpoints.establishments, validateToken, EstablishmentsController.add)
-    .delete(endpoints.establishments, validateToken, EstablishmentsController.del)
-    .patch(endpoints.establishments, validateToken, EstablishmentsController.manageStore)
+    .get(Endpoints.establishments,  EstablishmentsController.findAll, paginationAndFilters)
+    .get(`${Endpoints.establishments}/:id`, EstablishmentsController.findOne)
+    .post(Endpoints.establishments, validateToken, EstablishmentsController.add)
+    .delete(Endpoints.establishments, validateToken, EstablishmentsController.del)
+    .put(Endpoints.establishments, validateToken, EstablishmentsController.update)
+    // .patch(Endpoints.establishments, validateToken, EstablishmentsController.manageStore)
 
 export default router;
