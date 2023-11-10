75727746fbb2f194b64ea905de0ecd92a8739cda
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-11-09 22:31:52 -0300

    Correção devido a warning

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const url = process.env.DB_URL;
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(url)
+
 
 let db = mongoose.connection;
 
