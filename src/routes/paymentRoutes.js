221281157de912244a198748e95d170ba0e3cb87
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-11-09 22:29:36 -0300

    Adicionando novos endpoints

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,13 +3,17 @@ import PaymentController from '../controllers/payments/paymentController.js';
 import Endpoints from "../models/Endpoints.js";
 import validateToken from "../middlewares/tokenController.js";
 import paginationAndFilters from "../middlewares/paginationAndFilters.js";
+import PaymentsApi from "../controllers/paymentsApi/paymentsApi.js";
 
 const router = express.Router();
 
 const paymentControl = new PaymentController();
+const paymentApi = new PaymentsApi();
 
 router
     .get(Endpoints.payments, validateToken, paymentControl.findAll, paginationAndFilters)
+    .post(`${Endpoints.payments}/create_charge`, paymentApi.createCharge)
+    .get(`${Endpoints.payments}/get_charge`, paymentApi.getCharge)
     .get(`${Endpoints.payments}/:id`, validateToken, paymentControl.findOne)
     .post(Endpoints.payments, validateToken, paymentControl.add)
     .delete(Endpoints.payments, validateToken, paymentControl.rollbackPayments)
