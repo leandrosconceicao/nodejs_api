3890583a872a759f69109ac98e046bb4f6ed7bb2
Leandro <leandrosantosconceicao@gmail.com>
2023-11-19 18:19:20 -0300

    Refatorações

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import app from './src/app.js';
+import appWs from "./src/app-ws.js";
 import {createServer} from 'http';
-import { Server } from "socket.io";
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, {})
+
 
 const port = process.env.PORT || 80
 
 
 httpServer.listen(port, "0.0.0.0")
 
-export default io;
+appWs(httpServer);
+// export default io;
