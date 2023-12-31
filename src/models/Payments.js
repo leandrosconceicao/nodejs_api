8d066b8f5a5fd6f44ab83164dfd7eb9864ac77ba
Leandro <leandrosantosconceicao@gmail.com>
2023-12-31 14:45:43 -0300

    Refatoração necessária para reaproveitamento de schema de pagamentos

--- src/models/Payments.js
+++ src/models/Payments.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 var ObjectId = mongoose.Types.ObjectId;
 
 const paymentSchema = new mongoose.Schema({
+    // _id: {type: ObjectId},
     accountId: {
         type: ObjectId, ref: 'accounts'
     },
     refunded: {type: Boolean, default: false},
     storeCode: {type: mongoose.Types.ObjectId, ref: "establishments", required: [true, "Parametro (storeCode) é obrigatório"]},
     userCreate: {type: ObjectId, ref: "users", required: [true, "Parametro (userCreate) é obrigatório"]},
-    createDate: {type: Date, required: [true, "Parametro (createDate) é obrigatório"]},
+    createDate: {type: Date, default: new Date(), required: [true, "Parametro (createDate) é obrigatório"]},
     userUpdated: {type: ObjectId, ref: "users",},
     updateDate: {type: Date},
     value: {
@@ -23,4 +24,4 @@ const paymentSchema = new mongoose.Schema({
 
 const Payments = mongoose.model("payments", paymentSchema);
 
-export default Payments;
+export {Payments, paymentSchema};
