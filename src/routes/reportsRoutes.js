f09709be608c462b51059c6b10a98bc75bf2ad37
Leandro <leandrosantosconceicao@gmail.com>
2024-01-12 23:09:29 -0300

    Rota para buscar os dados de tempo medio de preparo

--- src/routes/reportsRoutes.js
+++ src/routes/reportsRoutes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router
     .get(Endpoints.quantSales, validateToken, ReportsControllers.quantifySales)
     .get(Endpoints.quantSalesByProduct, validateToken, ReportsControllers.quantifySalesByProduct)
+    .get(`${Endpoints.reports}/average_preparation_time`, ReportsControllers.averagePreparationTime)
 
 export default router;
