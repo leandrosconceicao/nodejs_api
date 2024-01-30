a5200ab5394d56825b4e70b739633c9ec53499c6
Leandro <leandrosantosconceicao@gmail.com>
2024-01-29 21:57:18 -0300

    Novas rotas

--- src/routes/cardPaymentRoutes.js
+++ src/routes/cardPaymentRoutes.js
@@ -4,7 +4,9 @@ import CardPaymentController from "../controllers/payments/cardPaymentController
 const endpoint = "/card_payment";
 
 export default express.Router()
+    .get(`${endpoint}/check/:id`, CardPaymentController.checkPayment)
     .get(`${endpoint}/:id`, CardPaymentController.getPayment)
+    .post(`${endpoint}/webhook`, CardPaymentController.webhook)
     .post(endpoint, CardPaymentController.createPayment)
     .delete(endpoint, CardPaymentController.cancelPayment)
     .patch(`${endpoint}/device_operation_mode`, CardPaymentController.setDeviceOperationMode)
